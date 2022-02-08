8d74e1bd73310d5bbf3c367a8c0b12e4a3399393
Zoe Weinstein <mxzweinstein@gmail.com>
2022-02-07 21:09:41 -0700

    third test encountered null reference error.
    co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- ScrabbleCounter.Tests/ModelTests/WordTest.cs
+++ ScrabbleCounter.Tests/ModelTests/WordTest.cs
@@ -21,6 +21,39 @@ namespace ScrabbleCounter.Tests
       string inputtedWord = "word";
       Assert.AreEqual(inputtedWord, newWord.UserInput);
     }
+    [TestMethod]
+    public void GetLetterPointDictionary_RetrieveLetterPointDictionary_Dictionary()
+    {
+      Word newWord = new Word("word");
+      Dictionary<string, int> testLetterPoints = new Dictionary<string, int>();
+      testLetterPoints.Add("a", 1);
+      testLetterPoints.Add("b", 3);
+      testLetterPoints.Add("c", 3);
+      testLetterPoints.Add("d", 2);
+      testLetterPoints.Add("e", 1);
+      testLetterPoints.Add("f", 4);
+      testLetterPoints.Add("g", 2);
+      testLetterPoints.Add("h", 4);
+      testLetterPoints.Add("i", 1);
+      testLetterPoints.Add("j", 8);
+      testLetterPoints.Add("k", 5);
+      testLetterPoints.Add("l", 1);
+      testLetterPoints.Add("m", 3);
+      testLetterPoints.Add("n", 1);
+      testLetterPoints.Add("o", 1);
+      testLetterPoints.Add("p", 3);
+      testLetterPoints.Add("q", 10);
+      testLetterPoints.Add("r", 1);
+      testLetterPoints.Add("s", 1);
+      testLetterPoints.Add("t", 1);
+      testLetterPoints.Add("u", 1);
+      testLetterPoints.Add("v", 4);
+      testLetterPoints.Add("w", 4);
+      testLetterPoints.Add("x", 8);
+      testLetterPoints.Add("y", 4);
+      testLetterPoints.Add("z", 10);
+      CollectionAssert.AreEqual(testLetterPoints, newWord.LetterPoints);
+    }
   }
 }
 
